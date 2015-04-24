ee861a7388dc74b65f8d75cf178c0ef88c2df305
Dominik Britz <dominik.britz@gmail.com>
2015-04-24 23:17:29 +0200

    Initial release

---
+++ Remote-GPUpdate.ps1
@@ -0,0 +1,38 @@
+﻿<#
+.SYNOPSIS
+This is a simple PowerShell script to execute gpuptade /target:computer on another computer
+
+.PARAMETER ComputerName
+Computer on which the command should run
+
+.EXAMPLE
+.\Remote-GPUpdate.ps1 -ComputerName Computer01
+
+.EXAMPLE
+"Computer01","Computer02" | .\Remote-GPUpdate.ps1
+
+.EXAMPLE
+Computerlist.txt | .\Remote-GPUpdate.ps1
+
+.INPUTS
+This script accepts pipeline input
+
+.OUTPUTS
+This script does not generate output
+
+.NOTES
+https://github.com/DominikBritz
+#>
+[CmdletBinding()]
+PARAM
+(
+    [Parameter(ValueFromPipeline=$True)]
+    [ValidateNotNullOrEmpty()]
+    [string]$ComputerName = $env:COMPUTERNAME
+)
+
+If (Test-Connection -ComputerName $ComputerName -Count 1 -Quiet)
+{
+    Write-Output "Processing $ComputerName"
+    Invoke-WmiMethod -class Win32_process -name Create -ArgumentList "cmd.exe /c gpupdate /target:computer" -ComputerName $ComputerName | out-null
+}
