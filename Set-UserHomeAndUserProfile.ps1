984883f30166e4a7bfd6c5d1657e41bfb79c93c6
Dominik Britz <DominikBritz@users.noreply.github.com>
2015-05-06 10:12:34 +0200

    Update Set-UserHomeAndUserProfile.ps1

--- Set-UserHomeAndUserProfile.ps1
+++ Set-UserHomeAndUserProfile.ps1
@@ -1,9 +1,10 @@
+[CmdletBinding()]
 <#
     .SYNOPSIS
     Creates a user home and profiledirectory and sets the permissions for the user
 
     .PARAMETER Username
-    The sAMAccountname of the user
+    The sAMAccountname of the user. Input from pipeline is permitted
 
     .PARAMETER Domain
     The domain of the user. If nothing is specified, the scripts defaults to the domain of the user who is running the script
@@ -14,27 +15,38 @@
     .PARAMETER ProfileShare
     The path to the share where the profile direcotry should be created The share has to exist
 
+    .PARAMETER Force
+    Deletes all existing permissions of the user. Needs to be $true or $false
+
     .NOTES
     Author: Dominik Britz
     Source: https://github.com/DominikBritz
 #>
 
-[CmdletBinding()]
+
 PARAM
 (
-    [Parameter(Mandatory=$True,ValidateNotNullOrEmpty)]
-    $Username,
+    [Parameter(Mandatory=$True,ValueFromPipeline=$true)]
+    [ValidateNotNullOrEmpty()]
+    [string]$Username,
 
     [Parameter(Mandatory=$False)]
-    $Domain= $env:USERDOMAIN,
+    [ValidateNotNullOrEmpty()]
+    [string]$Domain= $env:USERDOMAIN,
 
-    [Parameter(Mandatory=$False,ValidateNotNullOrEmpty)]
+    [Parameter(Mandatory=$False)]
+    [ValidateNotNullOrEmpty()]
     [ValidateScript({Test-Path $_})]
-    $HomeShare,
+    [string]$HomeShare,
 
-    [Parameter(Mandatory=$False,ValidateNotNullOrEmpty)]
+    [Parameter(Mandatory=$False)]
+    [ValidateNotNullOrEmpty()]
     [ValidateScript({Test-Path $_})]
-    $ProfileShare
+    [string]$ProfileShare,
+
+    [Parameter(Mandatory=$False)]
+    [ValidateNotNullOrEmpty()]    
+    [bool]$Force = $False
 
 )
 
@@ -46,32 +58,72 @@ Function Set-DirAcl
         [string]$Username,
         [string]$Domain,
         [string]$Share,
-        [string]$AccessRule
+        $AccessRule,
+        [bool]$Force
     )
 
     If (-not (Test-Path $(Join-Path $Share $username)))
     {
-        New-Item -Path $Share -Name $username -ItemType Directory | out-null
-        $acl = Get-Acl $(Join-Path $Share $username)
-        $ace = New-Object System.Security.AccessControl.FileSystemAccessRule($AccessRule)
-        $acl.AddAccessRule($ace)
-        Set-Acl $(Join-Path $Share $username) -AclObject $acl           
+        try
+        {
+            Write-Verbose "The directory $(Join-Path $Share $username) does not exist. Create it."
+            New-Item -Path $Share -Name $username -ItemType Directory
+            Write-Verbose "Directory $(Join-Path $Share $username) successfully created"
+
+            Write-Verbose "Set permissions"
+            $acl = Get-Acl $(Join-Path $Share $username)
+            $ace = New-Object System.Security.AccessControl.FileSystemAccessRule $AccessRule
+            $acl.AddAccessRule($ace)
+            Set-Acl $(Join-Path $Share $username) -AclObject $acl
+            Write-Verbose "Permission successfully set"
+        }        
+        catch 
+        {
+            Write-Error $_.Exception.Message
+        }                  
     }
     Else
     {
-        $acl = Get-Acl $(Join-Path $Share $username)
-        $ace = New-Object System.Security.AccessControl.FileSystemAccessRule($AccessRule)
-        $acl.AddAccessRule($ace)
-        Set-Acl $(Join-Path $Share $username) -AclObject $acl    
+        try
+        {
+            Write-Verbose "The directory $(Join-Path $Share $username) already exists"
+            Write-Verbose "Set permissions"
+            $acl = Get-Acl $(Join-Path $Share $username)
+            $AlreadyPermissions = $False
+            Foreach ($item in $acl.Access)
+            {
+                If ($item.IdentityReference.Value -eq "$domain\$username")
+                {
+                    $AlreadyPermissions = $True
+                    If ($Force)
+                    {
+                        $acl.RemoveAccessRule($item)
+                        $AlreadyPermissions = $False
+                    } 
+                }
+            }
+            If (-not $AlreadyPermissions)
+            {
+                $ace = New-Object System.Security.AccessControl.FileSystemAccessRule $AccessRule
+                $acl.AddAccessRule($ace)
+            }
+            Set-Acl $(Join-Path $Share $username) -AclObject $acl
+        }
+        catch
+        {
+            Write-Error $_.Exception.Message        
+        }
     }
 }
 
 If ($HomeShare) {
-    [string]$AccessRule = "'$domain\$username', 'Modify, Synchronize', 'ContainerInherit, ObjectInherit', 'None', 'Allow'"
-    Set-DirAclr -Username $Username -Domain $Domain -Share $HomeShare
+    Write-Verbose "Processing home directory for user $Username at $HomeShare"
+    $AccessRule = "$domain\$username", 'Modify, Synchronize', 'ContainerInherit, ObjectInherit', 'None', 'Allow'
+    Set-DirAcl -Username $Username -Domain $Domain -Share $HomeShare -AccessRule $AccessRule -Force $Force
 }
 
 If ($ProfileShare) {
-    [string]$AccessRule = "'$domain\$username', 'Full', 'ContainerInherit, ObjectInherit', 'None', 'Allow'"
-    Set-DirAcl -Username $Username -Domain $Domain -Share $ProfileShare -AccessRule $AccessRule
+    Write-Verbose "Processing profile directory for user $Username at $ProfileShare"
+    $AccessRule = "$domain\$username", 'Full', 'ContainerInherit, ObjectInherit', 'None', 'Allow'
+    Set-DirAcl -Username $Username -Domain $Domain -Share $ProfileShare -AccessRule $AccessRule -Force $Force
 }
