70b5681580e78753c70e0de78fe6fb630b406a53
Dominik Britz <DominikBritz@users.noreply.github.com>
2019-03-29 11:38:27 +0100

    Update _TemplateScript.ps1

--- Scripts/_TemplateScript.ps1
+++ Scripts/_TemplateScript.ps1
@@ -112,13 +112,15 @@ param(
 if ($Verbose) { $VerbosePreference = 'Continue' }
 if ($Debug) { $DebugPreference = 'Continue' }
 
+[int]$BuildNumber = (Get-WmiObject Win32_OperatingSystem | Select-Object BuildNumber).BuildNumber
+
 Write-Debug "Command line is ___$($MyInvocation.Line)___"
 Write-Verbose 'Entering script body'
 
 If (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
   Write-Verbose 'Script is not run with administrative user'
 
-  If ((Get-WmiObject Win32_OperatingSystem | Select-Object BuildNumber).BuildNumber -ge 6000) {
+  If ($BuildNumber -ge 6000) {
     Write-Verbose 'Found UAC-enabled system. Elevating ...'
 
     $CommandLine = $MyInvocation.Line.Replace($MyInvocation.InvocationName, $MyInvocation.MyCommand.Definition)
