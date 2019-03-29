33073b9bfe5e0a158c3ed1f18bf7af8e243c61b7
Dominik Britz <DominikBritz@users.noreply.github.com>
2019-03-29 12:14:23 +0100

    Create Test-Admin.ps1

---
+++ Scripts/Test-Admin.ps1
@@ -0,0 +1,30 @@
+param(
+  [switch]$Verbose,
+  [switch]$Debug
+)
+
+if ($Verbose) { $VerbosePreference = 'Continue' }
+if ($Debug) { $DebugPreference = 'Continue' }
+
+[int]$BuildNumber = (Get-WmiObject Win32_OperatingSystem | Select-Object BuildNumber).BuildNumber
+
+Write-Debug "Command line is ___$($MyInvocation.Line)___"
+Write-Verbose 'Entering script body'
+
+If (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
+  Write-Verbose 'Script is not run with administrative user'
+
+  If ($BuildNumber -ge 6000) {
+    Write-Verbose 'Found UAC-enabled system. Elevating ...'
+
+    $CommandLine = $MyInvocation.Line.Replace($MyInvocation.InvocationName, $MyInvocation.MyCommand.Definition)
+    Write-Verbose "  $CommandLine"
+ 
+    Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList "$CommandLine"
+
+  } else {
+    Write-Verbose 'System does not support UAC'
+    Write-Warning 'This script requires administrative privileges. Elevation not possible. Please re-run with administrative account.'
+  }
+  Break
+}
