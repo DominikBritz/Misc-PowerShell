f4c8000f87f8340600c7615080c7815b93d4739f
Dominik Britz <DominikBritz@users.noreply.github.com>
2019-03-29 15:37:28 +0100

    Update Test-Admin.ps1

--- Scripts/Test-Admin.ps1
+++ Scripts/Test-Admin.ps1
@@ -1,30 +1,38 @@
-param(
-  [switch]$Verbose,
-  [switch]$Debug
-)
+Function Test-Admin
+{
+   PARAM
+   (
+     [switch]$Verbose,
+     [switch]$Debug
+   )
 
-if ($Verbose) { $VerbosePreference = 'Continue' }
-if ($Debug) { $DebugPreference = 'Continue' }
+   If ($Verbose) { $VerbosePreference = 'Continue' }
+   If ($Debug) { $DebugPreference = 'Continue' }
 
-[int]$BuildNumber = (Get-WmiObject Win32_OperatingSystem | Select-Object BuildNumber).BuildNumber
+   [int]$BuildNumber = (Get-WmiObject Win32_OperatingSystem | Select-Object BuildNumber).BuildNumber
 
-Write-Debug "Command line is ___$($MyInvocation.Line)___"
-Write-Verbose 'Entering script body'
+   Write-Debug "Command line is ___$($MyInvocation.Line)___"
+   Write-Verbose 'Entering script body'
 
-If (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
-  Write-Verbose 'Script is not run with administrative user'
+   If (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator'))
+   {
+      Write-Verbose 'Script is not run with administrative user'
 
-  If ($BuildNumber -ge 6000) {
-    Write-Verbose 'Found UAC-enabled system. Elevating ...'
+      If ($BuildNumber -ge 6000)
+      {
+         Write-Verbose 'Found UAC-enabled system. Elevating ...'
 
-    $CommandLine = $MyInvocation.Line.Replace($MyInvocation.InvocationName, $MyInvocation.MyCommand.Definition)
-    Write-Verbose "  $CommandLine"
- 
-    Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList "$CommandLine"
+         $CommandLine = $MyInvocation.Line.Replace($MyInvocation.InvocationName, $MyInvocation.MyCommand.Definition)
+         Write-Verbose "  $CommandLine"
 
-  } else {
-    Write-Verbose 'System does not support UAC'
-    Write-Warning 'This script requires administrative privileges. Elevation not possible. Please re-run with administrative account.'
-  }
-  Break
+         Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList "$CommandLine"
+
+      } 
+      Else 
+      {
+         Write-Verbose 'System does not support UAC'
+         Write-Warning 'This script requires administrative privileges. Elevation not possible. Please re-run with administrative account.'
+      }
+      Break
+   }
 }
